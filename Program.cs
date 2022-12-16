e52ec8188718b81c908873c92491a2b8a2f92917
Max Lefebvre <max.lefebvre@garvin-allen.com>
2022-12-16 14:38:35 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using InterBaseSql.Data.InterBaseClient;
+
+namespace InterbaseDialectDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = new IBConnectionStringBuilder
+            {
+                DataSource = "localhost",
+                Database = "C:\\YOUR_DATABASE_PATH.IB",
+                Dialect = 1, // This doesn't seem to be respected?
+                UserID = "sysdba",
+                Password = "masterkey",
+            };
+
+            // changing DATE to TIMESTAMP works
+            const string cmdText = "CREATE PROCEDURE TEST_PROCEDURE (TEST_ARG DATE) AS BEGIN EXIT; END";
+            using var connection = new IBConnection(connectionString.ToString());
+            using var command = new IBCommand(cmdText, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+
+            Console.WriteLine("Successfully created procedure!");
+            Console.ReadLine();
+        }
+    }
+}
